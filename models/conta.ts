c8607d21a44296d4bbeb4dfd0e6d12aaaeb74a37
Lucas Relva <lucas-1880@outlook.com>
2020-06-29 17:17:09 -0300

    Criando banco de dados e objeto modelo da conta!

---
+++ models/conta.ts
@@ -0,0 +1,15 @@
+ export = class transaction {
+     public id: number
+     public name: string
+     public type: string
+     public value: number
+
+     public constructor (id:number, name:string, type:string, value:number){
+
+        this.id = id
+        this.name = name
+        this.type = type
+        this.value = value
+        
+     }
+ }
