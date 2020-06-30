0dae94262690eafd69deb4e1caf4252580873fa4
Lucas Relva <lucas-1880@outlook.com>
2020-06-30 20:12:45 -0300

    Página das transações concluída, pastas de static files configurada no servidor, edição de estilo com bootstrap e css e estilo responsivo

--- server.ts
+++ server.ts
@@ -1,14 +1,23 @@
 import express = require('express')
 import path = require('path')
+import { appendFile } from 'fs'
 
 const server = express()
+
+server.set("views", path.join(__dirname, "/views"))
+
+server.use(express.static(path.join(__dirname, "/public"), {
+    cacheControl: true,
+    etag: false,
+    maxAge: "30d"
+}))
 
 server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/mytransactions.html'))
 })
 
- const porta = (parseInt(process.env.PORT) || 8000)
+const porta = (parseInt(process.env.PORT) || 8000)
 
- server.listen(porta, () => {
-     console.log('Executando o servidor na porta:' +porta)
- })
+server.listen(porta, () => {
+    console.log('Executando o servidor na porta:' + porta)
+})
