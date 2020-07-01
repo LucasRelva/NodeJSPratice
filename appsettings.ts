20a8bd0c9d9fc47410290ded5ad73729a4b7bb89
Lucas Relva <lucas-1880@outlook.com>
2020-07-01 19:12:01 -0300

    Configurando SQL

---
+++ appsettings.ts
@@ -0,0 +1,13 @@
+export = {
+
+    sqlPool: {
+
+        connectionLimit: 30,
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'conta'
+
+    }
+}
