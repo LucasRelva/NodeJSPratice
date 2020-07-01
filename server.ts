bfce0b8181a93415e4c81a180fba17d2c9ccda44
Lucas Relva <lucas-1880@outlook.com>
2020-07-01 17:58:50 -0300

    Rotas do servidor e view engine

--- server.ts
+++ server.ts
@@ -1,20 +1,34 @@
 import express = require('express')
 import path = require('path')
-import { appendFile } from 'fs'
+import ejs = require('ejs')
+import lru = require('lru-cache')
 
 const server = express()
 
-server.set("views", path.join(__dirname, "/views"))
+server.set('views', path.join(__dirname, '/views'))
 
-server.use(express.static(path.join(__dirname, "/public"), {
+server.use(express.static(path.join(__dirname, '/public'), {
     cacheControl: true,
     etag: false,
-    maxAge: "30d"
+    maxAge: '30d'
 }))
 
-server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/mytransactions.html'))
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }));
+
+ejs.cache = lru(200)
+
+server.set('view engine', 'ejs')
+server.use(require('express-ejs-layouts'))
+
+server.use((req, res, next) => {
+    res.header('Cache-Control', "private, no-cache, no-store, must-revalidate");
+	res.header("Expires", "-1");
+	res.header("Pragma", "no-cache");
+	next();
 })
+
+server.use('/', require('./routes/home'))
 
 const porta = (parseInt(process.env.PORT) || 8000)
 
