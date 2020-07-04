9d7792a63114e6b1d9186e6dcc35f44f580f77f8
Lucas Relva <lucas-1880@outlook.com>
2020-07-04 01:38:33 -0300

    Mudança do formulário

--- public/js/mytransactions.js
+++ public/js/mytransactions.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     $('.money').mask('000.000.000.000.000,00', { reverse: true });
 })
+
+$('.modal').on('hidden.bs.modal', function () {
+    $(this).find('form')[0].reset();
+})
+
+    
